824aae90d55bfec1041fdeb58241425a0eb78790
rafly.nagachi <rafly.nagachi@shopee.com>
2022-08-18 21:35:34 +0700

    fix: payload cache

--- middleware/request_validator.go
+++ middleware/request_validator.go
@@ -3,10 +3,14 @@ package middleware
 import (
 	"git.garena.com/sea-labs-id/batch-01/rafly-nagachi/final-project-backend/httperror"
 	"github.com/gin-gonic/gin"
+	"reflect"
 )
 
 func RequestValidator(model any) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		modelPtr := reflect.ValueOf(model).Elem()
+		modelPtr.Set(reflect.Zero(modelPtr.Type()))
+		c.Set("payload", new(any))
 		if err := c.ShouldBindJSON(model); err != nil {
 			appErr := httperror.BadRequestError(err.Error())
 			c.AbortWithStatusJSON(appErr.StatusCode, appErr)
