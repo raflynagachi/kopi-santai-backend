5bdf3ccb66aab741e911af554f320943918e30ec
rafly.nagachi <rafly.nagachi@shopee.com>
2022-08-20 17:36:39 +0700

    refactor: add role in UserJWT

--- dto/user_jwt.go
+++ dto/user_jwt.go
@@ -5,11 +5,13 @@ import "git.garena.com/sea-labs-id/batch-01/rafly-nagachi/final-project-backend/
 type UserJWT struct {
 	ID    uint   `json:"id"`
 	Email string `json:"email"`
+	Role  string `json:"role"`
 }
 
 func (_ *UserJWT) FromUser(u *model.User) *UserJWT {
 	return &UserJWT{
 		ID:    u.ID,
 		Email: u.Email,
+		Role:  u.Role,
 	}
 }
