5542eac8be3432fbf897ebb41938ab2826dcba99
rafly.nagachi <rafly.nagachi@shopee.com>
2022-08-24 19:37:30 +0700

    fix: binding user update request

--- dto/user_update_req.go
+++ dto/user_update_req.go
@@ -5,5 +5,5 @@ type UserUpdateReq struct {
 	Phone          string `json:"phone" binding:"omitempty,e164,min=5,max=15"`
 	Address        string `json:"address"`
 	Email          string `json:"email" binding:"omitempty,email"`
-	ProfilePicture []byte `json:"profilePicture" binding:"omitempty,contains=data:image/"`
+	ProfilePicture []byte `json:"profilePicture"`
 }
