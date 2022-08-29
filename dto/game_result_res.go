955beaaac39d10be4b0ad1f099361aac15df1ded
rafly.nagachi <rafly.nagachi@shopee.com>
2022-08-29 08:32:25 +0700

    fix: json tag

--- dto/game_result_res.go
+++ dto/game_result_res.go
@@ -3,9 +3,9 @@ package dto
 import "git.garena.com/sea-labs-id/batch-01/rafly-nagachi/final-project-backend/model"
 
 type UserCouponRes struct {
-	ID        uint `json:"id"`
-	UserRes   *UserRes
-	CouponRes *CouponRes
+	ID        uint       `json:"id"`
+	UserRes   *UserRes   `json:"userRes"`
+	CouponRes *CouponRes `json:"couponRes"`
 }
 
 func (_ *UserCouponRes) From(uc *model.UserCoupon) *UserCouponRes {
